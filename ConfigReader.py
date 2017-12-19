ff6b4a469af3caced3a7d22d82c0d0f89093f73c
extramask93 <jozwiak.damian02@gmail.com>
2017-12-19 09:24:23 +0100

    added config reader

---
+++ ConfigReader.py
@@ -0,0 +1,19 @@
+import configparser as ConfigParser
+
+class ConfigReader:
+    def __init__(self,file):
+        self.Config = ConfigParser.ConfigParser()
+        self.Config.read(file)
+    def GetSectionMap(self,section): 
+        dict1 = {}
+        options = self.Config.options(section)
+        for option in options:
+            try:
+                dict1[option] = self.Config.get(section, option)
+                if dict1[option] == -1:
+                    DebugPrint("skip: %s" % option)
+            except:
+                print("exception on %s" % option)
+                dict1[option] = None
+        return dict1
+    
